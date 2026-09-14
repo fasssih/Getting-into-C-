2352451c3c51c154274d54e59c0d3b2c6307dfc7
Fasih <sultanffyt09@gmail.com>
2026-09-14 18:19:52 +0500

    Tic tac toe game completed but this is where the coding is literally ununderstandable and getting complicated

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,9 +2,144 @@
 #include <ctime>
 using namespace std;
 
+void drawboard(char *spaces);
+void playermove(char *spaces, char player);
+void computermove(char *spaces, char computer);
+bool checkwinner(char *spaces, char player, char computer);
+bool checktie(char *spaces);
 
 
 int main()
 {
+    char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    char player = 'X';
+    char computer = 'O';
+    srand(time(0));
+    bool running = true;
+    drawboard(spaces);
+    while (running)
+    {
+        playermove(spaces,player);
+        drawboard(spaces);
+        if(checkwinner(spaces,player,computer)){
+            running = false;
+            break;
+        }else if(checktie(spaces)){
+            running = false;
+            break;
+        }
+
+        computermove(spaces,computer);
+        drawboard(spaces);
+        if(checkwinner(spaces,player,computer)){
+            running = false;
+            break;
+        }else if(checktie(spaces)){
+            running = false;
+            break;
+        }
+    }
+    cout<<"Thanks for playing!";
     
-} 
+    
+}
+void drawboard(char *spaces)
+{
+    cout << "\n";
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << '\n';
+    cout << "_____|_____|_____" << '\n';
+
+    cout << "     |     |     \n";
+    cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << '\n';
+    cout << "_____|_____|_____" << '\n';
+
+    cout << "     |     |     \n";
+    cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << '\n';
+    cout << "     |     |     " << '\n';
+}
+void playermove(char *spaces, char player){
+    int number;
+
+    do
+    {
+        cout << "Enter a spot to place a marker (1-9): ";
+        cin >> number;
+        if (number >= 1 && number <= 9)
+        {
+            number--;  // Convert 1-9 into array index 0-8.
+
+            if (spaces[number] == ' ')
+            {
+                spaces[number] = player;
+                break;
+            }
+            else
+            {
+                cout << "That spot is already taken!\n";
+            }
+        }
+        else
+        {
+            cout << "Please enter a number from 1 to 9.\n";
+        }
+
+    } while (true);
+}
+void computermove(char *spaces, char computer){
+    int number;
+    while (true)
+    {
+        number = rand() % 9;
+
+        if (spaces[number] == ' ')
+        {
+            spaces[number] = computer;
+            break;
+        }
+    }
+}
+bool checkwinner(char *spaces, char player, char computer){
+    if(spaces[0] != ' ' && spaces[0] == spaces[1] && spaces[1] == spaces[2]){
+        spaces[0] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    if(spaces[3] != ' ' && spaces[3] == spaces[4] && spaces[4] == spaces[5]){
+        spaces[3] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    if(spaces[6] != ' ' && spaces[6] == spaces[7] && spaces[7] == spaces[8]){
+        spaces[6] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    if(spaces[0] != ' ' && spaces[0] == spaces[3] && spaces[3] == spaces[6]){
+        spaces[0] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    if(spaces[1] != ' ' && spaces[1] == spaces[4] && spaces[4] == spaces[7]){
+        spaces[1] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    if(spaces[2] != ' ' && spaces[2] == spaces[5] && spaces[5] == spaces[8]){
+        spaces[2] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    if(spaces[0] != ' ' && spaces[0] == spaces[4] && spaces[4] == spaces[8]){
+        spaces[0] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    if(spaces[6] != ' ' && spaces[6] == spaces[4] && spaces[4] == spaces[2]){
+        spaces[6] == player? cout<<"You won\n": cout<<"Computer won!\n";
+        return true;
+    }
+    return false;
+}
+bool checktie(char *spaces){
+    for(int i =0; i<9; i++){
+        if(spaces[i] == ' '){
+            return false;
+        } 
+    }
+    cout<<"IT'S a TIE!";
+    return true;
+}
