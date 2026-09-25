db223be7fff8ced12fb3498d71d1d53896810714
Fasih <sultanffyt09@gmail.com>
2026-09-25 20:02:22 +0500

    Practicing C++

--- Tutorial 1/Practicin.cpp
+++ Tutorial 1/Practicin.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <ctime>
+#include <cmath>
 using namespace std;
 
 /*Commits on Aug 10, 2026*/
@@ -12,6 +12,8 @@ namespace first{
 typedef std::string ors_t;
 int main()
 {
+    
+    /*
     //Commits on Aug 9, 2026
     ors_t chalo;
     cout<<"Enter your first word!";
@@ -25,15 +27,39 @@ int main()
     cout<<"\n"<<first::x;
     
     //Commits on Aug 11,12 2026
-    int y;
+    double y;
     cout<<"\nEnter your marks obtained: ";
     cin>>y;
     string name;
-    cout<<"\nEnter your full name: ";
+    cout<<"Enter your full name: ";
     getline(cin>>ws,name);
     double per = (y/510) *100;
-    cout<<'\n'<<name<<" Got "<<per<<"% in his matriculatoin";
-
+    cout<<name<<" Got "<<per<<"% in his matriculation";
+*/
     //Commits on Aug 13, 2026
+
+    double x = 3.4;
+    double y = 5.3;
+    double z,n;
+    //z = max(x,y);
+    //n = min(x,y);
+    //n = pow(x,y);
+    //z = sqrt(y);
+    //n = ceil(x);
+    //z = floor(y);
+    //n = abs(-34);
+    //z = round(y);
+    //cout<< "Here is the absolute "<<n<<" Here is the round off result "<<z;
+    double a,b;
+    cout<<"Enter the first side of the right angled triangle: ";
+    cin>>a;
+    cout<<"Enter the second side of the right angled triangle: ";
+    cin>>b;
+    double c =sqrt(a*a + b*b);
+    cout<<"Here is the third side: "<<c;
+
+
+
+    // Fasih gift center change contacts shorte 
     
 }  
